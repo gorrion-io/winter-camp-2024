03c40b58c52b341e5ccca613aa9f27e8c83b160e
xyashino <szczypiorpan123@gmail.com>
2023-12-27 22:22:47 +0100

    feat(crew-SSR): implement pagination for crew list using SSR

--- pages/task/[page].tsx
+++ pages/task/[page].tsx
@@ -3,6 +3,26 @@
  * @description Use tanstack/react-query or swr to fetch data from the endpoint. Prepare pagination.
  */
 
-export default function Task() {
-  return <div>Task</div>;
+import { Pagination } from "@/components/pagination/Pagination";
+import { CrewList } from "@/components/crew-list/CrewList";
+import { getCrew } from "@/lib/api/get-crew";
+import { parseToNumber } from "@/lib/utils/parse-to-number";
+import { NextPageContext } from "next";
+
+export const getServerSideProps = async ({
+  query: { page },
+}: NextPageContext) => {
+  const parsedPage = parseToNumber(page);
+  if (!parsedPage) return { notFound: true };
+  const data = await getCrew({ page: parsedPage });
+  return { props: data };
+};
+
+export default function Task({ data, pagination }: CrewResponse) {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-around items-center overflow-hidden">
+      <CrewList crew={data} />
+      <Pagination {...pagination} />
+    </div>
+  );
 }
