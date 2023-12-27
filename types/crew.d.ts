54e56775d9eafec0c504d2cc319573ee467776a5
xyashino <szczypiorpan123@gmail.com>
2023-12-27 15:24:18 +0100

    refactor: update and streamline crew types
    - Revise crew type definitions for clarity and precision.
    - Remove redundant and unused crew type declarations.

--- types/crew.d.ts
+++ types/crew.d.ts
@@ -4,18 +4,3 @@ type CrewMember = {
     age: number;
     profession: string;
 };
-
-type JsonCrewMember = {
-    firstName: string;
-    lastName: string;
-    nationality: string;
-    age: number;
-    profession: string;
-};
-
-type YamlCrewMember = {
-    name: string;
-    nationality: string;
-    years_old: number;
-    occupation: string;
-};
