5074aef47f4bcc894a475d3cc4cdc7c8da56dd9a
xyashino <szczypiorpan123@gmail.com>
2023-12-27 22:14:34 +0100

    feat(validation): add pages for handling form validation errors

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Custom404() {
+  return (
+    <div className="flex min-h-screen flex-col place-content-center place-items-center p-24">
+      <div className="flex place-items-center">
+        <h1 className="text-4xl font-bold text-center">404 - Page Not Found</h1>
+      </div>
+      <div className="flex place-items-center mt-12">
+        <Link href="/">Go home</Link>
+      </div>
+    </div>
+  );
+}
---
+++ pages/_error.tsx
@@ -0,0 +1,32 @@
+import { NextPage, NextPageContext } from "next";
+import Link from "next/link";
+
+interface Props {
+  statusCode?: number;
+  message?: string;
+}
+
+const Error: NextPage<Props> = ({ statusCode , message }) => {
+  return (
+    <div className="flex min-h-screen flex-col place-content-center place-items-center p-24">
+      <div className="flex flex-col space-y-2 place-items-center">
+        <h1 className="text-4xl font-bold text-center">
+          {statusCode
+            ? `An error ${statusCode} occurred on server`
+            : "An error occurred on client"}
+        </h1>
+          {message && <p>{message}</p>}
+      </div>
+      <div className="flex place-items-center mt-12">
+        <Link href="/">Go home</Link>
+      </div>
+    </div>
+  );
+};
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode , message: err?.message};
+};
+
+export default Error;
