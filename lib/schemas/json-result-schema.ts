6de2c8f16f70e830432089e590eee6b34e3faca8
xyashino <szczypiorpan123@gmail.com>
2023-12-27 14:21:09 +0100

    feat: add schemas for crew member data validation

---
+++ lib/schemas/json-result-schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export type JsonCrewMember = z.infer<typeof jsonCrewMemberSchema>;
+export type JsonResult = z.infer<typeof jsonResultSchema>;
+
+
+const jsonCrewMemberSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  nationality: z.string(),
+  age: z.number().int().positive(),
+  profession: z.string(),
+});
+
+export const jsonResultSchema = z.array(jsonCrewMemberSchema);
+
---
+++ lib/schemas/yaml-result-schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export type YamlCrewMember = z.infer<typeof yamlCrewMemberSchema>;
+export type YamlResult = z.infer<typeof yamlResultSchema>;
+
+const yamlCrewMemberSchema = z.object({
+  name: z.string(),
+  occupation: z.string(),
+  years_old: z.number().int().positive(),
+  nationality: z.string(),
+});
+
+export const yamlResultSchema = z.array(yamlCrewMemberSchema);
+
