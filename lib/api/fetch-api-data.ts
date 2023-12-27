619eee3c582572cda024257aa92920011985334f
xyashino <szczypiorpan123@gmail.com>
2023-12-27 22:19:15 +0100

    feat(crew-fetch): implement fetch method for crew data retrieval

---
+++ lib/api/fetch-api-data.ts
@@ -0,0 +1,31 @@
+import {APP_DOMAIN} from "@/config/constants";
+
+interface FetchOptions extends Omit<RequestInit, "method"> {
+  path: string;
+  query?: Record<string, any>;
+  method?:
+    | "GET"
+    | "POST"
+    | "PUT"
+    | "DELETE"
+    | "PATCH"
+}
+
+export const fetchApiData = async({
+  path,
+  headers,
+  query,
+  method = "GET",
+  ...fetchOptions
+}: FetchOptions) => {
+  const queryStr = new URLSearchParams(query).toString();
+  const url = `${path}${queryStr ? `?${queryStr}` : ""}`;
+  return fetch(`${APP_DOMAIN}${url}`, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    ...fetchOptions,
+  });
+};
---
+++ lib/api/get-crew.ts
@@ -0,0 +1,25 @@
+import { DEFAULT_TAKE_ITEMS } from "@/config/constants";
+import { fetchApiData } from "@/lib/api/fetch-api-data";
+
+interface GetCrewPayload {
+  page?: number;
+  take?: number;
+}
+
+export const getCrew = async ({
+  page = 1,
+  take = DEFAULT_TAKE_ITEMS,
+}: GetCrewPayload) => {
+  if (page <= 0 || isNaN(page) || isNaN(take)) return;
+  const res = await fetchApiData({
+    path: "/api/crew",
+    query: {
+      take,
+      page,
+    },
+  });
+  const result = await res.json();
+
+  if (res.ok) return result as CrewResponse;
+  throw Error(result.message || "Something went wrong try again later.")
+};
