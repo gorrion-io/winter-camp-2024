9768dec16c705f7086ff13b7f5c711d32b45a694
xyashino <szczypiorpan123@gmail.com>
2023-12-27 13:31:03 +0100

    feat: add TypeScript definition file for crew types
    - Create `crew.d.ts` to define types related to crew members.

---
+++ types/crew.d.ts
@@ -0,0 +1,21 @@
+type CrewMember = {
+    fullName: string;
+    nationality: string;
+    age: number;
+    profession: string;
+};
+
+type JsonCrewMember = {
+    firstName: string;
+    lastName: string;
+    nationality: string;
+    age: number;
+    profession: string;
+};
+
+type YamlCrewMember = {
+    name: string;
+    nationality: string;
+    years_old: number;
+    occupation: string;
+};
