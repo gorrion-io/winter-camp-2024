9eac3fef97985dbb96113c24fed78662ed5b9dcc
bushmeen <bushmeen@gmail.com>
2023-12-28 17:48:18 +0100

    made custom 404 page

--- components/errors/CrewInformationError.tsx
+++ components/errors/CrewInformationError.tsx
@@ -11,8 +11,8 @@ export const CrewInformationError = ({ error }: Props) => {
 	const router = useRouter();
 
 	return (
-		<div className=' w-full mt-20  flex flex-col justify-center items-center text-center'>
-			<h1 className='text-2xl'>Ops... Something went wrong</h1>
+		<div className=' w-full mt-20  flex flex-col justify-center items-center text-center gap-6'>
+			<h1 className='text-2xl md:text-4xl font-semibold'>Ops... Something went wrong</h1>
 			<p className='text-lg text-muted-foreground'>{error?.message}</p>
 
 			<div className='flex items-center gap-4 mt-10'>
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,24 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/router';
+
 const Custom404 = () => {
+	const router = useRouter();
 	return (
-		<div className='w-full h-full flex justify-center items-center'>
-			<h1>404 - Page Not Found</h1>
+		<div className='w-full mt-20  flex flex-col justify-center items-center text-center gap-6'>
+			<h1 className='text-6xl lg:text-8xl font-bold  tracking-widest'>404</h1>
+			<h2 className='text-2xl md:text-4xl font-semibold'>Oops, your force is not strong enough.</h2>
+			<div className='sm:text-lg text-muted-foreground'>
+				<p>This page you requested could not be found.</p>
+				<p>May the force be with you!</p>
+			</div>
+			<Button
+				size={'lg'}
+				onClick={() => {
+					router.push('/');
+				}}>
+				Back home
+			</Button>
 		</div>
 	);
 };
