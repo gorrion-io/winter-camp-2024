711c7c9711c7898f64fdfb01819718e6a6e39b16
Fabi0o <marfab32@gmail.com>
2023-12-28 18:05:33 +0100

    feat: add react-query and set fetching data with it

--- pages/task/[page].tsx
+++ pages/task/[page].tsx
@@ -3,6 +3,34 @@
  * @description Use tanstack/react-query or swr to fetch data from the endpoint. Prepare pagination.
  */
 
+import { CrewResponse } from "@/types/crewMember";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+
 export default function Task() {
-  return <div>Task</div>;
+  const router = useRouter();
+  const page = router.query.page;
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["crew", page],
+    queryFn: async (): Promise<CrewResponse> => {
+      const res = await fetch(`/api/crew?page=${page}`);
+      if (!res.ok) throw new Error("Something went wrong!");
+
+      return res.json();
+    },
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>{`${error}`}</div>;
+
+  const crew = data!.paginatedCrewList;
+
+  return (
+    <div>
+      {crew.map((member, i) => (
+        <div key={i}>{member.fullName}</div>
+      ))}
+    </div>
+  );
 }
