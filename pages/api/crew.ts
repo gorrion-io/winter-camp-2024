f497a2017997e30208f1e4e3ec5ce3659c0859f1
Fabi0o <marfab32@gmail.com>
2023-12-28 17:31:15 +0100

    feat: creat endpoint for fetching paginated crew list

--- pages/api/crew.ts
+++ pages/api/crew.ts
@@ -1,10 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import joinCrew from "@/lib/crew";
+import { CrewMember } from "@/types/crewMember";
+
+type ResponseData = {
+  paginatedCrewList: CrewMember[];
+  originalListLength: number;
+};
 
 /**
  * @todo Prepare an endpoint to return a list of crew members
  * @description The endpoint should return a pagination of 8 users per page. The endpoint should accept a query parameter "page" to return the corresponding page.
  */
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json([]);
+const paginateList = (pageNum: number, list: CrewMember[]) =>
+  list.slice((pageNum - 1) * 8, pageNum * 8);
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const { page } = req.query;
+  const crewList = joinCrew();
+
+  res.status(200).json({
+    paginatedCrewList: paginateList(Number(page), crewList),
+    originalListLength: crewList.length,
+  });
 }
