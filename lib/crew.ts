33c6f4fff71cf229ca54f905e7823fc2c5f20f6b
Bartosz <bartek.104@o2.pl>
2023-12-30 18:26:42 +0100

    Added method that returns list of crew members

--- lib/crew.ts
+++ lib/crew.ts
@@ -2,3 +2,61 @@
  * @todo Prepare a method to return a list of crew members
  * @description The list should only include crew members aged 30 to 40
  */
+import fs from "fs";
+import yaml from "js-yaml";
+
+type JsonMember = {
+  firstName: string;
+  lastName: string;
+  nationality: string;
+  age: number;
+  profession: string;
+};
+
+type YamlMember = {
+  name: string;
+  nationality: string;
+  years_old: number;
+  occupation: string;
+};
+
+type CrewMember = {
+  fullName: string;
+  nationality: string;
+  age: number;
+  profession: string;
+};
+
+const chosenCrew: () => Promise<CrewMember[]> = async () => {
+  const dataPath = process.cwd();
+  const file = await fs.promises.readFile(dataPath + "/crew.json", "utf8");
+  const jsonMembers: JsonMember[] = await JSON.parse(file);
+
+  const crewMembers: CrewMember[] = jsonMembers.map((member) => {
+    const mappedMember: CrewMember = {
+      age: member.age,
+      fullName: `${member.firstName} ${member.lastName}`,
+      nationality: member.nationality,
+      profession: member.profession,
+    };
+    return mappedMember;
+  });
+  try {
+    const yamlMembers = yaml.load(
+      fs.readFileSync(dataPath + "/crew.yaml", "utf8")
+    ) as YamlMember[];
+    yamlMembers.forEach((member) => {
+      const mappedMember: CrewMember = {
+        age: member.years_old,
+        fullName: member.name,
+        nationality: member.nationality,
+        profession: member.occupation,
+      };
+      crewMembers.push(mappedMember);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  return crewMembers.filter((member) => member.age >= 30 && member.age <= 40);
+};
+export default chosenCrew;
--- pages/api/crew.ts
+++ pages/api/crew.ts
@@ -1,10 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
+import chosenCrew from "../../lib/crew";
 /**
  * @todo Prepare an endpoint to return a list of crew members
  * @description The endpoint should return a pagination of 8 users per page. The endpoint should accept a query parameter "page" to return the corresponding page.
  */
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json([]);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const lists = await chosenCrew();
+  res.status(200).json(lists);
 }
