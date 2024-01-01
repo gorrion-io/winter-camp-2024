a4d9b14f5bdad206928fa8ce13e1c3fcb327196d
Szymon Kedra <s5zee@wp.pl>
2024-01-01 20:48:37 +0100

    invalid file type check removed

--- lib/crew.ts
+++ lib/crew.ts
@@ -1,18 +1,16 @@
-import { CrewMember, JsonYamlArray } from "@/types/apiDataTypes";
-import * as fs from "fs";
-import * as yaml from "js-yaml";
+import { CrewMember, JsonYamlArray } from '@/types/apiDataTypes'
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
 
 export const mergeCrew = () => {
   const readDataFile = (filePath: string) => {
-    const fileContent = fs.readFileSync(filePath, "utf-8");
-    if (filePath.endsWith(".json")) {
-      return JSON.parse(fileContent);
-    } else if (filePath.endsWith(".yaml")) {
-      return yaml.load(fileContent);
-    } else {
-      throw new Error("Invalid file type");
+    const fileContent = fs.readFileSync(filePath, 'utf-8')
+    if (filePath.endsWith('.json')) {
+      return JSON.parse(fileContent)
+    } else if (filePath.endsWith('.yaml')) {
+      return yaml.load(fileContent)
     }
-  };
+  }
 
   const concatenateData = (crewMembers: JsonYamlArray[]) =>
     crewMembers.map(
@@ -31,21 +29,21 @@ export const mergeCrew = () => {
         age: age || years_old,
         profession: profession || occupation,
       })
-    );
+    )
 
   const filterCrewMembers = (crewMembers: CrewMember[]) =>
-    crewMembers.filter(({ age }) => age >= 30 && age <= 40);
+    crewMembers.filter(({ age }) => age >= 30 && age <= 40)
 
   const sortCrewMembers = (filteredCrewMembers: CrewMember[]) =>
     filteredCrewMembers.sort((a, b) =>
       a.fullName.toUpperCase().localeCompare(b.fullName.toUpperCase())
-    );
+    )
 
-  const jsonCrewMembers = readDataFile("crew.json");
-  const yamlCrewMembers = readDataFile("crew.yaml");
-  const crewMembers = concatenateData([...jsonCrewMembers, ...yamlCrewMembers]);
-  const filteredCrewMembers = filterCrewMembers(crewMembers);
-  const sortedCrewMembers = sortCrewMembers(filteredCrewMembers);
+  const jsonCrewMembers = readDataFile('crew.json')
+  const yamlCrewMembers = readDataFile('crew.yaml')
+  const crewMembers = concatenateData([...jsonCrewMembers, ...yamlCrewMembers])
+  const filteredCrewMembers = filterCrewMembers(crewMembers)
+  const sortedCrewMembers = sortCrewMembers(filteredCrewMembers)
 
-  return sortedCrewMembers;
-};
+  return sortedCrewMembers
+}
