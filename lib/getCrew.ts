69a09a55c340f408c8556b2b4b06f09d76bf9402
Szymon Kedra <s5zee@wp.pl>
2024-01-01 11:07:16 +0100

    getCrew function added for react query

---
+++ lib/getCrew.ts
@@ -0,0 +1,16 @@
+import { ApiResponse } from "@/types/apiDataTypes";
+import { wait } from "./wait";
+
+export const getCrew = async ({ queryKey }: any) => {
+  await wait(500);
+
+  const [, { page }] = queryKey;
+
+  const res = await fetch(`/api/crew?page=${page}`);
+
+  if (!res.ok) throw new Error(res.statusText);
+
+  const data = (await res.json()) as ApiResponse;
+
+  return data;
+};
