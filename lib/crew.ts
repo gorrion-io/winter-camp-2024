d98e597706fd0e3902ffd93ca872adac968e9159
Szymon Kedra <s5zee@wp.pl>
2024-01-01 11:06:39 +0100

    Created 4 functions readDataFile, concatenateData, filterCrewMembers sortCrewMembers inside of the mergeCrew function

--- lib/crew.ts
+++ lib/crew.ts
@@ -1,4 +1,51 @@
-/**
- * @todo Prepare a method to return a list of crew members
- * @description The list should only include crew members aged 30 to 40
- */
+import { CrewMember, JsonYamlArray } from "@/types/apiDataTypes";
+import * as fs from "fs";
+import * as yaml from "js-yaml";
+
+export const mergeCrew = () => {
+  const readDataFile = (filePath: string) => {
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+    if (filePath.endsWith(".json")) {
+      return JSON.parse(fileContent);
+    } else if (filePath.endsWith(".yaml")) {
+      return yaml.load(fileContent);
+    } else {
+      throw new Error("Invalid file type");
+    }
+  };
+
+  const concatenateData = (crewMembers: JsonYamlArray[]) =>
+    crewMembers.map(
+      ({
+        firstName,
+        lastName,
+        name,
+        age,
+        years_old,
+        nationality,
+        profession,
+        occupation,
+      }) => ({
+        fullName: name || `${firstName} ${lastName}`,
+        nationality,
+        age: age || years_old,
+        profession: profession || occupation,
+      })
+    );
+
+  const filterCrewMembers = (crewMembers: CrewMember[]) =>
+    crewMembers.filter(({ age }) => age >= 30 && age <= 40);
+
+  const sortCrewMembers = (filteredCrewMembers: CrewMember[]) =>
+    filteredCrewMembers.sort((a, b) =>
+      a.fullName.toUpperCase().localeCompare(b.fullName.toUpperCase())
+    );
+
+  const jsonCrewMembers = readDataFile("crew.json");
+  const yamlCrewMembers = readDataFile("crew.yaml");
+  const crewMembers = concatenateData([...jsonCrewMembers, ...yamlCrewMembers]);
+  const filteredCrewMembers = filterCrewMembers(crewMembers);
+  const sortedCrewMembers = sortCrewMembers(filteredCrewMembers);
+
+  return sortedCrewMembers;
+};
