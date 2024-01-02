9b6bb18980a093ef7caa9b74fc054d906ce002ae
Bartosz <bartek.104@o2.pl>
2024-01-02 03:29:26 +0100

    Added loading skeleton and styles

--- pages/task/[page].tsx
+++ pages/task/[page].tsx
@@ -4,10 +4,10 @@
  */
 import { useRouter } from "next/router";
 import { CrewMember } from "@/lib/crew";
-import Link from "next/link";
 import useSWR from "swr";
 import Card from "@/components/Card";
 import Button from "@/components/Button";
+import CardSkeleton from "@/components/CardSkeleton";
 
 async function fetcher(
   url: string
@@ -18,17 +18,21 @@ async function fetcher(
 
 export default function Task() {
   const router = useRouter();
+  let pageQuery = router.query.page;
+  const { data, error, isLoading } = useSWR(
+    `/api/crew?page=${pageQuery}`,
+    fetcher
+  );
 
-  const page = Number(router.query.page);
-  const { data, error, isLoading } = useSWR(`/api/crew?page=${page}`, fetcher);
-  if (isLoading)
-    return (
-      <div className="flex justify-center h-screen items-center text-2xl">
-        Loading...
-      </div>
-    );
-  if (!data?.paginatedList) return <div>No data</div>;
-  if (page < 1 || page > data!.totalPages || isNaN(page)) {
+  let page: number | undefined =
+    typeof pageQuery === "string" ? Number(pageQuery) : undefined;
+
+  if (typeof page === "undefined") return null;
+
+  if (isLoading && typeof page === "number" && page > 0) {
+    return <CardSkeleton />;
+  }
+  if (!data || page < 1 || page > data.totalPages || isNaN(page)) {
     return (
       <div className="flex justify-center items-center h-screen flex-col gap-4">
         <span className="text-2xl font-bold">Page not found</span>
@@ -36,19 +40,20 @@ export default function Task() {
       </div>
     );
   }
+
   if (error) return <div>Error: {error.message}</div>;
   return (
     <main className="m-4 mt-20">
       <ul className="flex gap-4 flex-wrap justify-center items-center">
         {data.paginatedList.map((member, idx) => {
-          return <Card key={idx + 8 * (page - 1)} member={member} />;
+          return <Card key={idx + 8 * (page! - 1)} member={member} />;
         })}
       </ul>
       <section className="flex gap-4 justify-center mt-4">
         {Number(router.query.page) === 1 ? null : (
           <Button pageNumber={Number(router.query.page)} label="Previous" />
         )}
-        {Number(router.query.page) === data.totalPages ? null : (
+        {Number(router.query.page) === data?.totalPages ? null : (
           <Button pageNumber={Number(router.query.page)} label="Next" />
         )}
       </section>
