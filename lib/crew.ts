66164faf3f3cbd734b9ca258e4dca29b90cf63af
Michał Bebłociński <94801124+Michalbeblocinski@users.noreply.github.com>
2024-01-04 00:21:20 +0100

    task completed

--- lib/crew.ts
+++ lib/crew.ts
@@ -1,4 +1,48 @@
-/**
- * @todo Prepare a method to return a list of crew members
- * @description The list should only include crew members aged 30 to 40
- */
+import fs from 'fs';
+import path from 'path';
+// @ts-ignore
+import yaml from 'js-yaml';
+
+type CrewMember = {
+    fullName: string;
+    nationality: string;
+    age: number;
+    profession: string;
+};
+
+type FileType = "json" | "yaml";
+
+type DataParams = {
+    type:FileType | string
+    path:string
+}
+
+const sortBetweenAge = (member: CrewMember) => member.age >= 30 && member.age <= 40;
+
+const paginateAndFilterData = (data: CrewMember[], page: number): CrewMember[] => {
+    const itemsPerPage = 8;
+    const startIndex = (page - 1) * itemsPerPage;
+
+    return data.filter(sortBetweenAge).slice(startIndex, startIndex + itemsPerPage);
+}
+
+const getFileData = (item:DataParams, page:number) =>{
+    switch (item.type) {
+        case "json":
+            const jsonFile = fs.readFileSync(path.resolve(item.path), 'utf8');
+            const jsonData: CrewMember[] = JSON.parse(jsonFile);
+            return paginateAndFilterData(jsonData, page)
+        case "yaml":
+            const yamlFile = fs.readFileSync(path.resolve(item.path), 'utf8');
+            const yamlData: CrewMember[] = yaml.load(yamlFile) as CrewMember[];
+            return paginateAndFilterData(yamlData, page)
+        default:
+            return [];
+    }
+}
+
+export const astronautsList = (data: DataParams[], page: number): CrewMember[] => {
+    return data.reduce((acc: CrewMember[], val: DataParams) => {
+        return [...acc, ...getFileData(val, page)];
+    }, []);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,24 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-card': 'linear-gradient(71deg, #080509, #19213C, #080509)',
+        'gradient-card-after': ' linear-gradient(71deg, #0c0a0e, #7b8197, #0c0a0e)',
+      },
+      width: {
+        '74': '74%',
+        '63': '63%',
+        '100px': '100px',
+        '250px':'250px'
+      },
+      borderRadius:{
+        "999px":'999px'
+      },
+      height: {
+        '100px': '100px',
+        '250px':'250px'
+      },
+      backgroundColor: {
+        '19213C': '#19213C',
       },
     },
   },
