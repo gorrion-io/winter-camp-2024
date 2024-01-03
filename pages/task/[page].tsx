864154a7fef65144408621818bfcc0a18a1c5fa0
Aleksandra <elinda100@gmail.com>
2024-01-03 20:14:00 +0100

    added swr and improved styling

--- pages/task/[page].tsx
+++ pages/task/[page].tsx
@@ -3,37 +3,52 @@ import { useRouter } from "next/router";
 import { CrewMember } from "@/lib/definitions";
 import Link from "next/link";
 import MemberCard from "@/components/mui/MemberCard";
+import useSWR from "swr";
+import { Skeleton } from "@mui/material";
 
-const CrewList = () => {
+const fetcher = async (url: string) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch crew data");
+  }
+  const data = await response.json();
+  return data;
+};
+
+export default function CrewList() {
   const [crewMembers, setCrewMembers] = useState<CrewMember[]>([]);
   const [totalPages, setTotalPages] = useState<number>(1);
-
-  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const router = useRouter();
   const { page } = router.query;
 
+  const {
+    data: crewData,
+    error,
+    isValidating,
+  } = useSWR(`/api/crew?page=${page}`, fetcher);
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`/api/crew?page=${page}`);
-        if (!response.ok) {
-          throw new Error("Failed to fetch crew data");
-        }
-        const data = await response.json();
-        setCrewMembers(data["collection"]);
-        setTotalPages(data["totalPages"]);
-      } catch (error: any) {
-        setError(error.message);
-        router.replace("/");
-        router.reload();
-      }
-    };
+    if (crewData) {
+      setCrewMembers(crewData.collection || []);
+      setTotalPages(crewData.totalPages || 1);
+      setIsLoading(false);
+    }
+  }, [crewData]);
 
-    fetchData();
-  }, [page, router]);
+  useEffect(() => {
+    if (error) {
+      router.replace("/");
+      router.reload();
+    }
+  }, [error, router]);
 
-  return (
+  return isLoading ? (
+    <div className="w-screen h-screen flex justify-center">
+      <p className="text-lg">Loading...</p>
+    </div>
+  ) : (
     <div>
       <div className="w-11/12 mx-auto mt-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3 lg:gap-6">
         {crewMembers.map((member, index) => (
@@ -45,7 +60,7 @@ const CrewList = () => {
           <Link
             key={i}
             href={`/task/${i + 1}`}
-            className="mx-2 px-4 py-2 text-grey-50 text-xl"
+            className="mx-2 px-4 pt-2 pb-8 text-grey-50 text-xl"
           >
             {i + 1}
           </Link>
@@ -53,6 +68,4 @@ const CrewList = () => {
       </div>
     </div>
   );
-};
-
-export default CrewList;
+}
