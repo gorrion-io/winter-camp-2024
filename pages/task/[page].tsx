d92e9d822e75ba7e19b50956c493fe4cb4cd7175
MichalNiescior <michal.niescior1@gmail.com>
2024-01-04 21:13:07 +0100

    Install @tanstack/react-query and add fetch data from api/crew

--- pages/task/[page].tsx
+++ pages/task/[page].tsx
@@ -3,6 +3,42 @@
  * @description Use tanstack/react-query or swr to fetch data from the endpoint. Prepare pagination.
  */
 
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+
 export default function Task() {
-  return <div>Task</div>;
+  const router = useRouter();
+  const pageIndex = router.query.page;
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["crew", pageIndex],
+    queryFn: async () => {
+      const res = await fetch(`/api/crew?page=${pageIndex}`).then((res) => {
+        if (!res.ok) throw new Error("Page not found!");
+        return res.json();
+      });
+
+      return res;
+    },
+  });
+
+  if (isLoading)
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  if (error)
+    return (
+      <div>
+        <h2> An error has occurred: {error.message}</h2>
+      </div>
+    );
+
+  return (
+    <div>
+      <h2>Crew List</h2>
+    </div>
+  );
 }
