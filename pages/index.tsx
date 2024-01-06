7160e2757745204ae83a2a72c3264e8a18cc86f9
Vertori <115083330+Vertori@users.noreply.github.com>
2024-01-06 13:36:52 +0100

    Modified homepage, created custom error page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,21 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col place-content-center place-items-center p-24 ${inter.className}`}
     >
-      <div className="flex place-items-center">
-        <h1 className="text-4xl font-bold text-center">
-          Winter Camp 2024 Recruitment Task
-        </h1>
-      </div>
-      <div className="flex place-items-center mt-12">
-        <Link href="/task/1">Go to task</Link>
+      <div className="flex flex-col items-center justify-center p-8 shadow-2xl ">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-4xl font-bold text-center">
+            Winter Camp 2024 Recruitment Task
+          </h1>
+          <h2 className="text-2xl">by Rafał Fikus</h2>
+        </div>
+        <div className="flex mt-12 place-items-center">
+          <Link
+            href="/task/1"
+            className="p-6 text-lg text-white transition bg-blue-500 hover:bg-blue-600"
+          >
+            Go to task
+          </Link>
+        </div>
       </div>
     </main>
   );
